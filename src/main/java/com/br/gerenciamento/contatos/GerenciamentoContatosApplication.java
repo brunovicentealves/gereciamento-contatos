857a7715d1dd4043f89718d5a7c4a79bd608e5cf
Bruno Alves <brunova@primeit.com.br>
2023-06-23 12:02:48 -0300

    project initial

---
+++ src/main/java/com/br/gerenciamento/contatos/GerenciamentoContatosApplication.java
@@ -0,0 +1,13 @@
+package com.br.gerenciamento.contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/gerenciamento/contatos/GerenciamentoContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.gerenciamento.contatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
