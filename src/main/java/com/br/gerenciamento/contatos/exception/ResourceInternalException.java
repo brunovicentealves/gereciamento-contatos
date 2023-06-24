83f3bbd274a74a3d7c8ed28334bac00d8985b005
Bruno Alves <brunovicentealves.ti@gmail.com>
2023-06-24 20:23:23 -0300

    create handlerException

---
+++ src/main/java/com/br/gerenciamento/contatos/exception/ResourceInternalException.java
@@ -0,0 +1,11 @@
+package com.br.gerenciamento.contatos.exception;
+
+public class ResourceInternalException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceInternalException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ src/main/java/com/br/gerenciamento/contatos/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.br.gerenciamento.contatos.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+	
+	 private static final long serialVersionUID = 1L;
+
+	  public ResourceNotFoundException(String msg) {
+	    super(msg);
+	  }
+
+}
