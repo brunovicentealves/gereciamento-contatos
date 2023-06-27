1a6ff4efd88beaa74df9a9067b8301e43302c1e0
Bruno Alves <brunovicentealves.ti@gmail.com>
2023-06-27 19:45:59 -0300

    fit endpoints and contatoService

--- src/main/java/com/br/gerenciamento/contatos/controller/ControllerContatos.java
+++ src/main/java/com/br/gerenciamento/contatos/controller/ControllerContatos.java
@@ -1,65 +1,80 @@
 package com.br.gerenciamento.contatos.controller;
 
+import java.util.List;
+import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.br.gerenciamento.contatos.dtos.ContatoDTO;
+import com.br.gerenciamento.contatos.service.ContatoService;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/contato")
 public class ControllerContatos {
-	
-	private static Logger logger  = LoggerFactory.getLogger(ControllerContatos.class);
-	
+	@Autowired
+	private ContatoService service;
+
+	private static Logger logger = LoggerFactory.getLogger(ControllerContatos.class);
+
 	@ApiOperation(value = "Adciona novo contato.")
-	@PostMapping(value="/cadastro")
-	public  ResponseEntity<?>  cadastrarContato(@RequestBody ContatoDTO contato){
-		
+	@PostMapping("/cadastro")
+	public ResponseEntity<String> cadastrarContato(@RequestBody @Valid ContatoDTO contato) {
+
 		logger.info("Iniciando Cadastro de contatos ");
-		
-		return null;
+		String response = service.adcionarContato(contato);
+
+		logger.info("Finalizando Cadastro de contatos ");
+
+		return ResponseEntity.ok(response);
 	}
-	
-	
+
 	@ApiOperation(value = "Listar todos os contatos.")
-	@GetMapping(value="/listar")
-	public  ResponseEntity<?> listarTodosContatos(){
-		
+	@GetMapping(value = "/listar")
+	public ResponseEntity<?> listarTodosContatos() {
+
 		logger.info("Iniciando Listar de contatos ");
-		
-		return null;
+
+		List<ContatoDTO> responseContato = service.buscarTodosContatos();
+
+		logger.info("Finalizando Listar de contatos ");
+
+		return ResponseEntity.ok(responseContato);
 	}
-	
-	
-	
+
 	@ApiOperation(value = "Alterar contato especifico.")
-	@PutMapping(value= "/alterar")
-	public ResponseEntity<?> alterarContato(){
-		
+	@PutMapping(value = "/alterar/{id}")
+	public ResponseEntity<?> alterarContato(@RequestBody ContatoDTO contato, @PathVariable Long id ) {
+
 		logger.info("Iniciando Alterar contatos ");
 		
-		return null;
+		ContatoDTO responseContato = service.alterarContato(contato, id);
+		
+		logger.info("Finalizando Alterar contatos ");
+
+		return ResponseEntity.ok(responseContato);
 	}
-	
+
 	@ApiOperation(value = "Deletar contato especifico.")
-	@DeleteMapping(value = "/delete")
-	public ResponseEntity<String> deletarContato (@PathVariable Long id ){
-		
+	@DeleteMapping(value = "/delete/{id}")
+	public ResponseEntity<String> deletarContato(@PathVariable Long id) {
+
 		logger.info("Iniciando Deletar contatos ");
-		
-		
-		return ResponseEntity.ok("Deletado com sucesso Contato");
+
+		String messageResponse = service.deleteContato(id);
+
+		logger.info("Finalizando Deletar contatos ");
+
+		return ResponseEntity.ok(messageResponse);
 	}
-	
-	
 
 }
--- src/main/java/com/br/gerenciamento/contatos/dtos/ContatoDTO.java
+++ src/main/java/com/br/gerenciamento/contatos/dtos/ContatoDTO.java
@@ -1,22 +1,46 @@
 package com.br.gerenciamento.contatos.dtos;
 
 import java.time.LocalDate;
-import lombok.Builder;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import com.br.gerenciamento.contatos.model.Contato;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 
 @Data
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContatoDTO {
 	
+	@NotBlank(message="Nome não pode ser vazio !")
 	private String nome;
 	
+	@Email(message = "Email invalido!")
 	private String email;
 	
+	@NotBlank(message="Telefone não pode ser vazio !")
 	private String telefone;
 	
+	 @JsonFormat(pattern="dd/MM/yyyy")
 	private LocalDate  dataNascimento;
+	
+	
+	public ContatoDTO(Contato contato) {
+		super();
+		this.nome = contato.getNome();
+		this.email = contato.getEmail();
+		this.telefone = contato.getTelefone();
+		this.dataNascimento = contato.getDataNascimento();
+	}
+
+
+	
+	
+	
 	
 	
 
